f267283c79aaa269b312e1186f91e23eb5874a6a
AlexE1987 <evsikovad1987@gmail.com>
2021-03-12 11:44:04 +0400

    feat: add function - revers-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const reversNumber = String(n).split('').reverse().join('');
+  const reversMinusNumber = reversNumber.slice(0,-1);  
+
+    if(n >= 0) {
+        return Number(reversNumber);
+    } else {
+        return Number(reversMinusNumber);
+    };
 }
